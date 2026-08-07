769e9be75f0ef200c1cb4209f07e7f002530e8c9
MahirEEA <145217054+MahirEEA@users.noreply.github.com>
2026-08-08 00:31:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -61,20 +61,6 @@ function movementCycle() {
 bot.on('error', (err) => {
   console.error('⚠️ Error:', err);
 });
-
 bot.on('end', () => {
   console.log('⛔️ Bot Disconnected!');
-});
-
-// Web sunucusu en alta, bağımsız bir şekilde eklendi (Render'ın port uyarısını kesmek için)
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 12905;
-
-app.get('/', (req, res) => {
-  res.send('Bot is alive!');
-});
-
-app.listen(PORT, () => {
-  console.log(`HTTP server is running on port ${PORT}`);
 });
